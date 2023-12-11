11e02d9c62980997f7b4b9f5b5c2cdb4d7babd60
yigitthejava <82784013+ozaiithejava@users.noreply.github.com>
2023-12-11 03:14:52 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,25 @@
+public class ShutdownExample {
+
+    public static void main(String[] args) {
+        shutdownComputer();
+    }
+
+    public static void shutdownComputer() {
+        try {
+            String os = System.getProperty("os.name").toLowerCase();
+
+            if (os.contains("win")) {
+                // Windows işletim sistemi
+                Runtime.getRuntime().exec("shutdown.exe -s -t 0");
+            } else if (os.contains("nix") || os.contains("nux") || os.contains("mac")) {
+                // Unix veya MacOS işletim sistemi
+                Runtime.getRuntime().exec("shutdown -h now");
+            } else {
+                System.out.println("İşletim sistemi desteklenmiyor.");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
